d511513faaa7f29d113d56bcd146f43dfeba81c7
wathika <vcwathika@gmail.com>
2015-08-25 11:41:20 +0300

    before filling out the site

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'jobs/index'
+  #get 'jobs/index'
 
-  get 'jobs/new'
+  #get 'jobs/new'
+
+  resources :jobs
+  root 'jobs#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
