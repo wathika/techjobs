ea84b1ca06f793cb2c25e679034e352b3350b1fc
wathika <vcwathika@gmail.com>
2015-08-25 12:20:08 +0300

    created create page

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,5 +3,15 @@ class JobsController < ApplicationController
   end
 
   def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(params.require(:job).permit(:title, :company, :url))
+    if @job.save
+      redirect_to root_path
+    else
+      render "new"
+    end
   end
 end
